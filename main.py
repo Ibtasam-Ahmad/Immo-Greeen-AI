9b5e82aef0a4d441f7fb9558ab130c4e08a26645
Ibtasam Ahmad <85065814+Ibtasam-Ahmad@users.noreply.github.com>
2024-12-17 18:52:29 +0500

    API as input

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import streamlit as st
-from agent import generate_stream, generate_response
+from openai import OpenAI
 
-
+# Streamlit UI setup
 st.set_page_config(
-    page_title="Immo Greeen AI",
-    page_icon="🧑‍🍳",
+    page_title="Immo Green AI",
+    page_icon="🏡",
     menu_items={
         'About': "This app is a prototype made for demo purposes. It is not meant for real life use. Do not take anything from this app as advice."
     }
 )
 st.title("Immo Green AI")
 
-
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
     response = generate_response(st.session_state.messages)
     st.session_state.messages.append({"role": "assistant", "content": response})
-    
 
 # Display chat messages from history on app rerun
-# if len(st.session_state.messages) > 1:
 for message in st.session_state.messages:
     if message["role"] == "assistant":     
         with st.chat_message(name="assistant", avatar="👨‍⚕️"):
@@ -37,8 +34,10 @@ if prompt := st.chat_input("Message Chief-GPT"):
     with st.chat_message("user"):
         st.markdown(prompt)
 
-# Display assistant response in chat message container
+    # Display assistant response in chat message container
     with st.chat_message(name="assistant", avatar="👨‍⚕️"):
         stream = generate_stream(st.session_state.messages)
         response = st.write_stream(stream)
+    
+    # Append assistant response to session history
     st.session_state.messages.append({"role": "assistant", "content": response})
