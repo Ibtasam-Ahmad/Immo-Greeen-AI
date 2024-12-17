04f50b1d9490a37f4cffb712f5e015f2099d1d04
Ibtasam Ahmad <85065814+Ibtasam-Ahmad@users.noreply.github.com>
2024-12-17 18:49:04 +0500

    Immo Green Title Updated

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from agent import generate_stream, generate_response
 
 
 st.set_page_config(
-    page_title="Chief-GPT",
+    page_title="Immo Greeen AI",
     page_icon="🧑‍🍳",
     menu_items={
         'About': "This app is a prototype made for demo purposes. It is not meant for real life use. Do not take anything from this app as advice."
     }
 )
-st.title("Chief-GPT")
+st.title("Immo Green AI")
 
 
 # Initialize chat history
